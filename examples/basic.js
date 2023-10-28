212c577c7d729b96d0ee4324ed325ce67f04ccf2
Jesse Shawl <jesse@jesse.sh>
2023-10-28 13:04:47 -0500

    Email with attachments

--- examples/basic.js
+++ examples/basic.js
@@ -1,7 +1,15 @@
 const { email } = require('../')
+const mime = require('mime')
 
 email({
-    to: 'example@example.com',
-    subject: 'hello',
-    body: 'hey there from js'
+  to: 'example@example.com',
+  subject: 'hello',
+  body: 'hey there from js',
+  attachments: [
+    {
+      content: Buffer.from("Hello, World!").toString('base64'),
+      type: mime.getType("txt"),
+      filename: "example.txt"
+    }
+  ]
 })
--- test/email.test.js
+++ test/email.test.js
@@ -1,5 +1,6 @@
 const { email } = require('../')
 const axios = require("axios")
+const mime = require('mime')
 jest.mock('axios')
 axios.post.mockImplementation(() => Promise.resolve())
 
@@ -9,5 +10,36 @@ test('email makes an http request', () => {
         subject: 'abc',
         body: 'def'
     })
-    expect(axios.post).toHaveBeenCalled()
+    expect(axios.post).toHaveBeenCalledWith("https://www.cinotify.cc/api/notify",{
+        subject: 'abc',
+        body: 'def',
+        to: 'jesse@jesse.sh',
+    })
+})
+
+test('email with attachments', () => {
+    email({
+        to: 'jesse@jesse.sh',
+        subject: 'abc',
+        body: 'def',
+        attachments: [
+            {
+                content: Buffer.from("Hello, World!").toString('base64'),
+                type: mime.getType("txt"),
+                filename: "example.txt"
+            }
+        ]
+    })
+    expect(axios.post).toHaveBeenCalledWith("https://www.cinotify.cc/api/notify",{
+        subject: 'abc',
+        body: 'def',
+        to: 'jesse@jesse.sh',
+        attachments: [
+            {
+                "content": "SGVsbG8sIFdvcmxkIQ==",
+                "filename": "example.txt",
+                "type": "text/plain",
+            }
+        ]
+    })
 })
